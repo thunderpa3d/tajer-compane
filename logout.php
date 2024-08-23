f406225a72c219aae8c60ee5ab5c634a4d2f7621
thunderpa3d <thunderpa3d@gmail.com>
2024-08-23 17:03:56 +0300

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-session_unset(); // إزالة جميع المتغيرات من الجلسة
-session_destroy(); // تدمير الجلسة
-header("Location: sin.html"); // إعادة التوجيه إلى صفحة تسجيل الدخول
-exit();
-?>
